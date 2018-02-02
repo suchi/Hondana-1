de55508fc54617df15cd3f09a590ababff4ad807
Toshiyuki Masui <masui@pitecan.com>
2018-02-02 13:52:36 +0900

    partial rendering, locals など

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
-  match 'xxx' => 'welcome#index', :via => :get
 
-  match 'disp/:type/:price' => 'welcome#index', :via => :get
-  
-  match ':shelf' => 'shelf#show', :via => :get
+  #
+  # 本棚表示
+  #
+  # app/views/shelf/show.html.erb
+  # app/controllers/shelf_controller.rb
+  match ':shelfname' => 'shelf#show', :via => :get
+  match ':shelfname/' => 'shelf#show', :via => :get
 
   # match ':controller(/:action(/:id))(.:format)' => 'welcome#index', :via => :get
   # controller という名前は特別なのかも
   
   match ':controller/:action', :via => :get
-
-  #get 'welcome/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
