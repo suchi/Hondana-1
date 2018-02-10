285ce7546f566f7936f140c88e460f323326325b
Toshiyuki Masui <masui@pitecan.com>
2018-02-10 21:23:26 +0900

    サニタイズまわりの処理を修正

--- app/helpers/shelf_helper.rb
+++ app/helpers/shelf_helper.rb
@@ -5,15 +5,20 @@ module ShelfHelper
   @@ISBNPAT = /\d{9}[0-9X]/
   @@CGIPAT = /\w+\.cgi$/
 
+  def expand_tag___(s,shelfname)
+    s = s.to_s.
+        gsub(/<(\/?(b|i|br|p|ul|ol|li|dl|dt|dd|hr|pre|blockquote|del))/i,'LBRA!\1!').
+        gsub(/</,'&lt;').
+        gsub(/LBRA!([^!]*)!/,'<\1').
+        gsub(/\[\[([^\]]*)\]\]/){ link($1,shelfname.to_s) }
+  end
+
   def expand_tag(s,shelfname)
     s = s.to_s.
-#    gsub(/<(\/?(b|i|br|p|ul|ol|li|dl|dt|dd|hr|pre|blockquote|del))>/i,'LBRA!\1!RBRA').
-#    gsub(/</,'&lt;').
-#    gsub(/LBRA!([^!]*)!RBRA/,'<\1>').
-    gsub(/<(\/?(b|i|br|p|ul|ol|li|dl|dt|dd|hr|pre|blockquote|del))/i,'LBRA!\1!').
-    gsub(/</,'&lt;').
-    gsub(/LBRA!([^!]*)!/,'<\1').
-    gsub(/\[\[([^\]]*)\]\]/){ link($1,shelfname.to_s) }
+        gsub(/<(\/?(b|i|br|p|ul|ol|li|dl|dt|dd|hr|pre|blockquote|del))>/i,'LBRA!\1!>').
+        gsub(/</,'&lt;').
+        gsub(/LBRA!([^!]*)!/,'<\1').
+        gsub(/\[\[([^\]]*)\]\]/){ link($1,shelfname.to_s) }
   end
 
   def link(s,shelfname)
@@ -50,4 +55,16 @@ module ShelfHelper
       link_to desc, :controller=> 'bookshelf', :action => 'search', :q => desc
     end
   end
+end
+
+if $0 == __FILE__ then
+  include ShelfHelper
+  puts expand_tag("abc<d>efg",'xxx')
+  puts expand_tag("abc<dd>efg",'xxx')
+  puts expand_tag("abc<ddd>efg",'xxx')
+  puts expand_tag("abc<d >efg",'xxx')
+  puts expand_tag("abc<dd >efg",'xxx')
+  puts expand_tag("abc<ddd >efg",'xxx')
+  s = "そもそもユーザは何を求めてるのかよく考えろ、と言ってるだけに聞こえる...<br>各事例は面白いのだが、それをまとめた「理論」など意味があるのだろうか? <p> 「そもそもから考える」ことを「ジョブ」という言葉を使っているだけである。マットレスを買った人の話があったが、<a 'bcd'>彼はマットレスが欲しかったのではなくて快眠が欲しかっただけだ、みたいな。これいったことを「快眠というジョブをハイアする」のように表現しており、そういう考えを「ジョブ理論」と呼んでいるようだ。 <br> そういう考え方は正しいだろうが理論というほどのものか? あたりまえではないのか??"
+  puts expand_tag(s,'xxx')
 end
