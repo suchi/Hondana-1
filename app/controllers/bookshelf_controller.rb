0cd6001161a6713f8e3465c6a350f5f0f2ba0b7b
Toshiyuki Masui <masui@pitecan.com>
2018-02-11 20:01:47 +0900

    limit(10).order() でトップ画像表示

--- app/controllers/bookshelf_controller.rb
+++ app/controllers/bookshelf_controller.rb
@@ -5,7 +5,7 @@ class BookshelfController < ApplicationController
   def list
     # newentries = Entry.find(:all, :order => "modtime DESC", :limit => 10, :conditions => "NOT comment = ''")
     # newentries = Entry.where(:all, ).order => "modtime DESC", :limit => 10, :conditions => "NOT comment = ''")
-    newentries = Entry.order("modtime DESC")[0..9]
+    newentries = Entry.limit(10).order("modtime DESC")
     # _deletedな本棚の本も見えてしまう?
 
     render locals: { newentries: newentries }
