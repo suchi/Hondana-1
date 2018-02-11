00fef2cbbbbf2dac48df1a407c553d02088cc05a
Toshiyuki Masui <masui@pitecan.com>
2018-02-11 15:15:45 +0900

    Entry.all.order => Entry.order

--- app/controllers/bookshelf_controller.rb
+++ app/controllers/bookshelf_controller.rb
@@ -5,8 +5,8 @@ class BookshelfController < ApplicationController
   def list
     # newentries = Entry.find(:all, :order => "modtime DESC", :limit => 10, :conditions => "NOT comment = ''")
     # newentries = Entry.where(:all, ).order => "modtime DESC", :limit => 10, :conditions => "NOT comment = ''")
-    newentries = Entry.all.order("modtime DESC")[0..9]
-    # _deletedな本棚の本も見えてしまう
+    newentries = Entry.order("modtime DESC")[0..9]
+    # _deletedな本棚の本も見えてしまう?
 
     render locals: { newentries: newentries }
   end
