124397c467d189d54002684681bafacbd0747f79
Toshiyuki Masui <masui@pitecan.com>
2018-02-11 20:45:38 +0900

    新規更新本棚リスト

--- app/controllers/bookshelf_controller.rb
+++ app/controllers/bookshelf_controller.rb
@@ -5,7 +5,8 @@ class BookshelfController < ApplicationController
   def list
     newentries = Entry.limit(10).where.not(:comment => "").order("modtime DESC") # 10件だけ
     # _deletedな本棚の本も見えてしまう?
+    dispshelves = Shelf.limit(15).order("modtime DESC") # .where.not("NOT name like '%_deleted%'")
 
-    render locals: { newentries: newentries }
+    render locals: { newentries: newentries, dispshelves: dispshelves }
   end
 end
