58fbbe4f342e9123a300ad15977b3cffd84787b4
Toshiyuki Masui <masui@pitecan.com>
2018-08-16 08:24:18 +0900

    ...

--- app/controllers/shelf_controller.rb
+++ app/controllers/shelf_controller.rb
@@ -343,7 +343,7 @@ class ShelfController < ApplicationController
       return
     end
 
-    # return # SPAM対策するときはここでリターンしてしまう
+    return # SPAM対策するときはここでリターンしてしまう
 
     puts "newname = #{newname}"
     if newname == '' then
