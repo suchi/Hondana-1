20f799fcc8a14ea07ea70b4da490e93f4f1e4879
Toshiyuki Masui <masui@pitecan.com>
2018-08-15 15:33:48 +0900

    常識ライブラリ追加

---
+++ lib/joushiki.rb
@@ -0,0 +1,22 @@
+require 'json'
+require 'uri'
+require 'net/https'
+
+#
+# 常識問題と答のMD5を得る
+#
+def get_joushiki()
+  url = "https://joushiki.herokuapp.com/"
+  begin
+    uri = URI.parse(URI.escape(url)) # 何故かescape必要?
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    req = Net::HTTP::Get.new(uri.path)
+    res = http.request(req)
+    return ["", ""] unless res
+    JSON.parse(res.body)
+  rescue
+    return ["", ""]
+  end
+end
